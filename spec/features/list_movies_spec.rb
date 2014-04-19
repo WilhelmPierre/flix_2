5871f7c9fdeace4475afbc83db41f452bdd560ae
Wilhem Pierre <wilhempierre@MacBook-Pro.local>
2014-04-19 11:07:34 +0900

    Create migration for Movie Model and Add more expectations to make spec test pass

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -28,6 +28,11 @@ movie3 = Movie.create(title: "Spider-Man",
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
+
+    expect(page).to have_text(movie1.rating)
+    expect(page).to have_text(movie1.description[0..10])
+    expect(page).to have_text(movie1.released_on)
+    expect(page).to have_text("$318,412,101.00")
   end
 
 end
