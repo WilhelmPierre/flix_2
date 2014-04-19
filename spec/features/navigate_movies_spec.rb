275d25658004c80487ce5f4aeb8862d52f7b9387
Wilhem Pierre <wilhempierre@MacBook-Pro.local>
2014-04-19 13:24:09 +0900

    Refactor navigation spec test

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -1,13 +1,10 @@
     
 require 'spec_helper'
 
-describe "Navigating movies" do
+describe "Movie navigation" do
+
   it "allows navigation from the detail page to the listing page" do
-    movie = Movie.create(title: "Iron Man",
-                         rating: "PG-13",
-                         total_gross: 318412101.00,
-                         description: "Tony Stark builds an armored suit to fight the throes of evil",
-                         released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
 
@@ -15,17 +12,15 @@ describe "Navigating movies" do
 
     expect(current_path).to eq(movies_path)
   end
+
   it "allows navigation from the listing page to the detail page" do
-  movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes)
 
-  visit movies_url
+    visit movies_url
 
-  click_link movie.title
+    click_link movie.title
 
-  expect(current_path).to eq(movie_path(movie))
- end
+    expect(current_path).to eq(movie_path(movie))
+  end
+
 end
