347d87b907ac7c96993ccc763cc42eeb0c78a61f
Wilhem Pierre <wilhempierre@MacBook-Pro.local>
2014-04-19 09:40:49 +0900

    Initial commit and modify gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix2::Application.config.secret_key_base = '0809fe9b49209b4a429192f1e96a5ec1438f4ba5baa5ea99f996626ad5d2d30653c1611b305d7c5f0cd30322fb7278fad73f0ca01552b9a8d5278937e36ad9e2'
