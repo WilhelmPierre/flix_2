a94cec053dc0d53c1b14f28e68cc65e300be8bdd
Wilhelm Pier <wilhempierre@MacBook-Pro.local>
2014-04-29 01:47:58 +0900

    Add new action controller and modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Flix2::Application.routes.draw do
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
   root "movies#index"
+  resources :movies
 end
