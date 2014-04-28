fa91239edd860c5fc74f9143613205e5b77a6c32
Wilhelm Pier <wilhempierre@MacBook-Pro.local>
2014-04-29 00:28:57 +0900

    write a spec for movie flop

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
-describe " Viewing an individual movie" do
- it "shows the movie's details" do
-  movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+describe "Viewing an individual movie" do
 
-  visit movie_url(movie)
+  it "shows the movie's details" do
+    movie = Movie.create(movie_attributes(total_gross: 300000000.00))
 
-  expect(page).to have_text(movie.title)
-  expect(page).to have_text(movie.rating)
-  expect(page).to have_text("$318,412,101.00")
-  expect(page).to have_text(movie.description)
-  expect(page).to have_text(movie.released_on)
+    visit movie_url(movie)
+
+    expect(page).to have_text(movie.title)
+    expect(page).to have_text(movie.rating)
+    expect(page).to have_text(movie.description)
+    expect(page).to have_text(movie.released_on)
   end
+  it "shows the total gross if the total gross exceeds $50M" do
+  movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+  
+  visit movie_url(movie)
+  
+  expect(page).to have_text("$60,000,000.00")
+  end
+  it "shows 'Flop!' if the total gross is less than $50M" do
+  movie = Movie.create(movie_attributes(total_gross: 40000000.00))
+ 
+ visit movie_url(movie)
+ 
+ expect(page).to have_text("Flop!")
+  end 
 end
