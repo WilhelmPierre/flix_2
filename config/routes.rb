9b238588d368df4f29ee0da040d8303f941e5397
Wilhelm Pier <wilhempierre@MacBook-Pro.local>
2014-04-29 01:14:07 +0900

    Add routes for edit spec and create form for review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flix2::Application.routes.draw do
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   root "movies#index"
 end
